9bd15d0d988d054a4301ddfdab91918025a0a246
Louis Poulain <louis.poulain-auzeau@epfl.ch>
2024-03-01 14:02:36 +0100

    change encodong

--- ai_models_graphcast/model.py
+++ ai_models_graphcast/model.py
@@ -282,18 +282,10 @@ class GraphcastModel(Model):
                 for data_var in output.data_vars:
                     encoding[data_var] = {
                     "original_shape": output[data_var].shape,
-                    "_FillValue": output[data_var].encoding.get(
-                        "_FillValue", -32767
-                    ),
+                    "_FillValue": -32767,
                     "dtype": np.int16,
-                    "add_offset": output[data_var].encoding.get(
-                        "add_offset", output[data_var].mean().compute().values
-                    ),
-                    "scale_factor": output[data_var].encoding.get(
-                        "scale_factor",
-                        output[data_var].std().compute().values
-                        / 1000, # save up to 32 std
-                    ),
+                    "add_offset": output[data_var].mean().compute().values,
+                    "scale_factor": output[data_var].std().compute().values / 1000, # save up to 32 std
                     # "zlib": True,
                     # "complevel": 5,
                     }
